ce7930a7f8ecf5d8ac724c871a1718aa1f5b0653
Katy St Sauveur <katyrogowski@yahoo.com>
2021-06-16 10:19:34 -0700

    Used attribute binding to add a URL

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Socks'
+            product: 'Socks',
+            image: './assets/images/socks_green.jpg',
+            url: 'https://bombas.com/products/womens-cushioned-no-shows?variant=green-%5Bhidden%5D&campignid=11193388074&adgroupid=108935785119&targetid=pla-1220862819267&matchtype=&network=u&device=c&keywords=&creative=467479942952&gclid=CjwKCAjwwqaGBhBKEiwAMk-FtH_kw5fTRD4IWDmaochnE1-N8vIU6ZkWaQwLz8gc2gsfDIFS-u_cvhoCwfQQAvD_BwE&size=l',
         }
     }
 })
